9c55b49dbd439e9f3b6a46eaf3726066c73be8ac
ahahn94 <ahahn94@outlook.com>
2017-06-22 21:25:18 +0200

    Added autoripFlac

--- autoripFlac.sh
+++ autoripFlac.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 CD=/dev/`echo $1|sed -e "s/.*\(sr[0-9]\).*/\\1/"`
-gnome-terminal -e "cd ~/Ripping && abcde -x -o flac -N -d //$CD"
+mkdir -p ~/Ripping
+cd ~/Ripping
+abcde -x -o flac -N -d //$CD
