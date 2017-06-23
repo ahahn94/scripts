26feec1af0c24ea027233a754fd8baac87645646
ahahn94 <ahahn94@outlook.com>
2017-06-23 09:32:09 +0200

    Update autoripFlac_install.sh

--- AutoripFLAC/autoripFlac_install.sh
+++ AutoripFLAC/autoripFlac_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-mkdir ~/Ripping
-cp autoripFlac.sh /usr/local/bin/
-cp autoripflac.desktop ~/.local/share/applications/
+mkdir ~/Ripping #Create directory for Ripping in your home directory
+sudo apt-get install abcde flac #Install the abcde cd ripper and the flac codec
+cp autoripFlac.sh /usr/local/bin/ #Copy the script
+cp autoripflac.desktop ~/.local/share/applications/ #Copy the application shortcut
 echo "You need to manualy set the default application for audio cds to AutoripFLAC, or else it will not work!"
+echo "ou can refer to https://www.howtogeek.com/117709/how-to-change-your-default-applications-on-ubuntu-4-ways/ if you need help setting up the default application. Have a look at the point Removable Media."
